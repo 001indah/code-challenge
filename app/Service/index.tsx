a695cf540ba6af70010598cfacff08922d23870a
001indah <indahbudiati001@gmail.com>
2024-05-21 17:06:49 +0700

    testimonials

--- app/Service/index.tsx
+++ app/Service/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Button from '../components/Hero/buttonPolos'
+import Testimonials from '../components/Testimonials'
 
 const Card = () => {
     return (
         <div>
+            <Testimonials />
             <div className="mx-5 lg:mx-20 overflow-hidden bg-white rounded-xl border shadow-lg">
                 <div className="flex flex-col overflow-hidden bg-white sm:flex-row ">
                     <div className="flex w-full flex-col p-4 sm:w-1/2 sm:p-8 lg:w-3/5">
                         <h2 className="text-xl font-bold text-black md:text-2xl lg:text-4xl">Contact Us<span className='text-hijauTulisan'> for Free Career Consultation!</span> </h2>
                         <p className="text-xs lg:text-sm mt-4  text-gray-700">If you are still uncertain about the career path you want to take, Purwadhika Career Network team is ready to assist you. Consultations are available every Monday to Thursday from 10:00-11:00 AM and 4:00-5:00 PM (WIB). <br /><br /> Fill in the data below to speak with our team.</p>
+
                         <form>
                             <div className="relative mt-2 w-full">
                                 <input type="text" id="email" value="email@gmail.com" className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
@@ -20,6 +23,7 @@ const Card = () => {
                             </div>
 
                         </form>
+
 
                         <Button href='https://wa.link/zdtc2y' className='hover:bg-hijauMuda mt-3 bg-black hover:text-black text-white text-base'>
                             Send
