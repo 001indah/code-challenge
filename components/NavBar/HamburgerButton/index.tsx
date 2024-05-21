9effa87e69d2c4d7dc3bae44320967518e98ee2c
001indah <indahbudiati001@gmail.com>
2024-05-21 10:42:58 +0700

    change button hamburger to div

--- components/NavBar/HamburgerButton/index.tsx
+++ components/NavBar/HamburgerButton/index.tsx
@@ -8,11 +8,11 @@ interface HamburgerButtonProps {
 
 const HamburgerButton: React.FC<HamburgerButtonProps> = ({ isNavbarFixed, isOpen, handleClick }) => {
     return (
-        <button onClick={handleClick} className='hamburger block absolute right-2 p-4 lg:hidden'>
+        <div onClick={handleClick} className='hamburger block absolute right-2 p-4 lg:hidden'>
             <span className={`w-[30px] h-[2px] my-2 block ${isNavbarFixed ? ' bg-black' : 'bg-white'}  ease-in-out duration-500 ${isOpen ? 'origin-top-left rotate-45' : ''}`}></span>
             <span className={`w-[30px] h-[2px] my-2 block ${isNavbarFixed ? ' bg-black' : 'bg-white'} ease-in-out duration-500 ${isOpen ? 'scale-0' : ''}`}></span>
             <span className={`w-[30px] h-[2px] my-2 block ${isNavbarFixed ? ' bg-black' : 'bg-white'} ease-in-out duration-500 ${isOpen ? 'origin-bottom-left -rotate-45' : ''}`}></span>
-        </button>
+        </div>
     );
 };
 
