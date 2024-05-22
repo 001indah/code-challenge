d465d47b0bc917b7ad5b70caaa7ee0de06f55832
001indah <indahbudiati001@gmail.com>
2024-05-22 11:15:36 +0700

    change img from unsplash to local image

--- app/Service/index.tsx
+++ app/Service/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Button from '../components/Hero/buttonPolos'
 import Testimonials from '../components/Testimonials'
+import Image from 'next/image'
+import smile from '@/public/smile.webp'
+
 
 const Card = () => {
     return (
@@ -14,11 +17,11 @@ const Card = () => {
 
                         <form>
                             <div className="relative mt-2 w-full">
-                                <input type="text" id="email" className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
+                                <input type="text" id="email" aria-label='email' className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
                                 <label className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600"> Enter Your Email </label>
                             </div>
                             <div className="relative mt-2 w-full">
-                                <input type="text" id="text" className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
+                                <input type="text" id="text" aria-label='message' className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
                                 <label className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600"> Enter Your Message</label>
                             </div>
 
@@ -31,7 +34,12 @@ const Card = () => {
                     </div>
 
                     <div className="order-first ml-auto h-48 w-full bg-gray-700 sm:order-none sm:h-auto sm:w-1/2 lg:w-2/5">
-                        <img className="h-full w-full object-cover" alt="smilel boys" src="https://images.unsplash.com/photo-1601933973783-43cf8a7d4c5f?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" loading="lazy" />
+                        <Image
+                            alt="smile boys"
+                            src={smile}
+                            className="h-full w-full object-cover"
+                        />
+                        {/* <img className="h-full w-full object-cover" alt="smilel boys" src="https://images.unsplash.com/photo-1601933973783-43cf8a7d4c5f?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" loading="lazy" /> */}
                     </div>
                 </div>
             </div>
